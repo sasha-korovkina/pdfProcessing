20b459ed8de3f1f8bfa5e4921c71ea6e90ac48f0
sasha <sasha@cmi2i.com>
2024-04-25 16:05:02 +0100

    update functions!

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import numpy as np
 import layoutparser as lp
 import random
 import cv2
+import matplotlib.pyplot as plt
 
 # Define the path to the image file
-image_file = "/mnt/c/Users/sasha/projects/business.jpg"
+# C:\Users\sasha\projects\pdfTest\CI Games Authorisation Letter 2023.jpg
+image_file = "/mnt/c/Users/sasha/projects/pdfTest/CI Games Authorisation Letter 2023.jpg"
+
+# Define the path to save the image
+output_image_file = "/mnt/c/Users/sasha/projects/pdfTest/CI Games Authorisation Letter 2023 - out.png"
 
 # Attempt to read the image
 image = cv2.imread(image_file)
@@ -15,11 +20,11 @@ image = cv2.imread(image_file)
 # Check if the image is read successfully
 if image is None:
     print("Error: Unable to read the image file. Please check if the file format is supported.")
-    exit()
+else:
+    plt.imshow(image)
+    plt.savefig(output_image_file)
+    print("Image saved to:", output_image_file)
 
-print('Image successfully read.')
-
-# Reverse the color channels to convert BGR to RGB
 image = image[..., ::-1]
 
 model = lp.Detectron2LayoutModel('lp://PubLayNet/faster_rcnn_R_50_FPN_3x/config',
@@ -27,17 +32,14 @@ model = lp.Detectron2LayoutModel('lp://PubLayNet/faster_rcnn_R_50_FPN_3x/config'
                                  label_map={0: "Text", 1: "Title", 2: "List", 3:"Table", 4:"Figure"})
 
 layout = model.detect(image)
-lp.draw_box(image, layout, box_width=3)
+img_with_boxes = lp.draw_box(image, layout, box_width=3)
+output_path = '/mnt/c/Users/sasha/projects/pdfTest/theFile.pdf'
+img_with_boxes.save(output_path)
+print("Image with layout saved at:", output_path)
 
 # model = lp.Detectron2LayoutModel('lp://HJDataset/faster_rcnn_R_50_FPN_3x/config')
 
-# images = convert_pdf_to_pil_images(pdf_no_img)
-#
-# image_arrays = [np.array(image) for image in images]
-# image_array = np.array(image_arrays)
-# image_array = image_array.astype(np.uint8)
-# print(image_array.shape)
-#
+
 # for i, img in enumerate(image_array):
 #     layout = model.detect(img)
 #     img_with_boxes = lp.draw_box(img, layout, box_width=3)
