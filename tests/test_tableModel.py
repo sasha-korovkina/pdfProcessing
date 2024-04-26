688c6844c3b9650c50b15f871fea3c2d1ffc8168
sasha <sasha@cmi2i.com>
2024-04-26 11:29:36 +0100

    font issue fix

--- tests/test_tableModel.py
+++ tests/test_tableModel.py
@@ -1,5 +1,5 @@
 import fitz
-from PIL import Image
+from PIL import Image, ImageFont
 import io
 import numpy as np
 import layoutparser as lp
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 image_file = "/mnt/c/Users/sasha/projects/pdfTest/sampleScannedPDFI.jpg"
 
 # Define the path to save the image
-output_image_file = "/mnt/c/Users/sasha/projects/pdfTest/sampleScannedPDFI.png"
+output_image_file = "/mnt/c/Users/sasha/projects/pdfTest/output/sampleScannedPDFI.png"
 
 # Attempt to read the image
-image = cv2.imread(image_file)
+image = plt.imread(image_file)
 
 # Check if the image is read successfully
 if image is None:
@@ -27,19 +27,15 @@ else:
 
 image = image[..., ::-1]
 
-model = lp.Detectron2LayoutModel('lp://TableBank/faster_rcnn_R_50_FPN_3x/config',
-                                 label_map={0: "Table"})
+# PubLayNet
+model = lp.Detectron2LayoutModel('lp://PrimaLayout/mask_rcnn_R_50_FPN_3x/config',
+                                 label_map={1:"TextRegion", 2:"ImageRegion", 3:"TableRegion", 4:"MathsRegion", 5:"SeparatorRegion", 6:"OtherRegion"})
 
 layout = model.detect(image)
 lp.elements.Layout
-text_blocks = lp.Layout([b for b in layout if b.type=='Text'])
-figure_blocks = lp.Layout([b for b in layout if b.type=='Figure'])
-text_blocks = lp.Layout([b for b in text_blocks \
-                   if not any(b.is_in(b_fig) for b_fig in figure_blocks)])
+font = ImageFont.load_default()
+img_with_boxes = lp.draw_box(image, layout, box_width=3, box_alpha = 0.5, show_element_type=True)
 
-
-img_with_boxes = lp.draw_box(image, layout, box_width=3)
-output_path = '/mnt/c/Users/sasha/projects/pdfTest/theFile.pdf'
+output_path = '/mnt/c/Users/sasha/projects/pdfTest/output/PrimaLayoutTest.pdf'
 img_with_boxes.save(output_path)
 print("Image with layout saved at:", output_path)
-
