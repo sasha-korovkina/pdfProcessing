193afb65b98b77b6710c8468d76165c8290fbc3c
sasha <sasha@cmi2i.com>
2024-04-30 12:09:27 +0100

    working

--- tests/test_pdfQuery.py
+++ tests/test_pdfQuery.py
@@ -4,20 +4,14 @@ from PyPDF2 import PdfWriter, PdfReader
 from reportlab.pdfgen import canvas
 import io
 import xml.etree.ElementTree as ET
+import math
 
 file_path = r"C:\Users\sasha\projects\pdfTest\input\Various 20230331 (2).pdf"
 output_pdf_path = r"C:\Users\sasha\projects\pdfTest\output\Various 20230331 (2).pdf"
 
 pdf = PDFQuery(file_path)
 pdf.load()
-
 pdf.tree.write(r"C:\Users\sasha\projects\pdfTest\output\outXML.xml", pretty_print=True)
-
-tree = ET.parse(r"C:\Users\sasha\projects\pdfTest\output\outXML.xml")
-root = tree.getroot()
-
-pdf_reader = PdfReader(r"C:\Users\sasha\projects\pdfTest\input\Various 20230331 (2).pdf")
-pdf_writer = PdfWriter()
 
 packet = io.BytesIO()
 can = canvas.Canvas(packet)
@@ -28,6 +22,9 @@ def annotate_pdf_with_text(xml_path, input_pdf_path, output_pdf_path):
 
     pdf_reader = PdfReader(input_pdf_path)
     pdf_writer = PdfWriter()
+
+    results_bene = []
+    results_pos = []
 
     for page in root.findall('.//LTPage'):
         page_num = int(page.get('page_index'))
@@ -46,16 +43,25 @@ def annotate_pdf_with_text(xml_path, input_pdf_path, output_pdf_path):
 
             text = elem.text.strip() if elem.text else ""
             if x0 == 17.65 and text:
+                results_bene.append([x0, y0, x1, y1, text])
                 print(f"** Match Found Bene ** Coordinates: ({x0}, {y0}, {x1}, {y1}) - Text: {text}")
-            if x0 == 368.15 and text:
+            if round(x1 * 10) / 10 == 403 and text:
+                results_pos.append([x0, y0, x1, y1, text])
                 print(f"** Match Found Position ** Coordinates: ({x0}, {y0}, {x1}, {y1}) - Position: {text}")
 
+
+            #[17.65, 505.303, 184.118, 514.253, 'Aviva Investors UK Fund Services Limited']
+            #[373.15, 385.303, 403.007, 394.252, '90,644']
             text_x = x0
             text_y = y0
             can.drawString(text_x, text_y, text)
-            if text:
-                print(text_x, text_y, x1, y1, text)
+        #     print(text_x, text_y, x1, y1, text)
+
+        print(results_pos)
+        print(results_bene)
         print("\n" + "=" * 40 + "\n")
+        results_bene = []
+        results_pos = []
 
         can.save()
         packet.seek(0)
