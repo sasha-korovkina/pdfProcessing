36439ab685737ce0df98eb452d88fdaf69e4a245
sasha <sasha@cmi2i.com>
2024-04-30 11:45:28 +0100

    font issue fix

--- tests/test_pdfQuery.py
+++ tests/test_pdfQuery.py
@@ -23,54 +23,47 @@ packet = io.BytesIO()
 can = canvas.Canvas(packet)
 
 def annotate_pdf_with_text(xml_path, input_pdf_path, output_pdf_path):
-    # Load the XML file
     tree = ET.parse(xml_path)
     root = tree.getroot()
 
-    # Load the PDF
     pdf_reader = PdfReader(input_pdf_path)
     pdf_writer = PdfWriter()
 
-    # Iterate over pages in the XML
     for page in root.findall('.//LTPage'):
         page_num = int(page.get('page_index'))
         pdf_page = pdf_reader.pages[page_num]
-        page_height = float(pdf_page.mediabox[3])  # The top edge of the PDF page
+        page_height = float(pdf_page.mediabox[3])
 
-        # Create a canvas to draw on
         packet = io.BytesIO()
         can = canvas.Canvas(packet, pagesize=(pdf_page.mediabox[2], page_height))
         can.setPageSize((pdf_page.mediabox[2], page_height))
 
-        # Draw rectangles and text
         for elem in page.findall('.//*[@bbox]'):
-            bbox = eval(elem.get('bbox'))  # Convert string representation to actual list
+            bbox = eval(elem.get('bbox'))
             x0, y0, x1, y1 = map(float, bbox)
 
-            # Adjust y-coordinates
-            adjusted_y0 = page_height - y1
-            adjusted_y1 = page_height - y0
-
-            # Draw the rectangle
             can.rect(x0, y0, x1 - x0, y1 - y0, stroke=1, fill=0)
 
-            # Retrieve and draw the text if available
             text = elem.text.strip() if elem.text else ""
-            text_x = x0
-            text_y = y0 # Center text vertically in the rectangle
-            can.drawString(text_x, text_y, text)
-            print(text_x, text_y, text)
+            if x0 == 17.65 and text:
+                print(f"** Match Found Bene ** Coordinates: ({x0}, {y0}, {x1}, {y1}) - Text: {text}")
+            if x0 == 368.15 and text:
+                print(f"** Match Found Position ** Coordinates: ({x0}, {y0}, {x1}, {y1}) - Position: {text}")
 
-        # Finalize the canvas and add it to the PDF
+            text_x = x0
+            text_y = y0
+            can.drawString(text_x, text_y, text)
+            if text:
+                print(text_x, text_y, x1, y1, text)
+        print("\n" + "=" * 40 + "\n")
+
         can.save()
         packet.seek(0)
         new_pdf = PdfReader(packet)
         new_page = new_pdf.pages[0]
         pdf_writer.add_page(new_page)
 
-    # Save the modified PDF
     with open(output_pdf_path, 'wb') as output_pdf:
         pdf_writer.write(output_pdf)
 
-# Usage
 annotate_pdf_with_text(r'C:\Users\sasha\projects\pdfTest\output\outXML.xml', r'C:\Users\sasha\projects\pdfTest\input\Various 20230331 (2).pdf', r'C:\Users\sasha\projects\pdfTest\output\Various 20230331 (2).pdf')
