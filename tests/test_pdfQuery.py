c56cee710903cd1abdfcef676083783fb1e2df64
sasha <sasha@cmi2i.com>
2024-05-01 09:03:53 +0100

    software producer found!

--- tests/test_pdfQuery.py
+++ tests/test_pdfQuery.py
@@ -9,6 +9,7 @@ import os
 import pandas as pd
 
 # C:\Users\sasha\projects\pdfTest\input\Various 20230331 (2).pdf
+# C:\Users\sasha\projects\pdfTest\input\Sample Current Disclosure Forms.pdf
 file_path = r"C:\Users\sasha\projects\pdfTest\input\Various 20230331 (2).pdf"
 output_pdf_path = r"C:\Users\sasha\projects\pdfTest\output\Various 20230331 (2).pdf"
 
@@ -39,6 +40,7 @@ def annotate_pdf_with_text(xml_path, input_pdf_path, output_pdf_path):
 
     results_bene = []
     results_pos = []
+    all_text = []
 
     for page in root.findall('.//LTPage'):
         page_num = int(page.get('page_index'))
@@ -56,6 +58,7 @@ def annotate_pdf_with_text(xml_path, input_pdf_path, output_pdf_path):
             can.rect(x0, y0, x1 - x0, y1 - y0, stroke=1, fill=0)
 
             text = elem.text.strip() if elem.text else ""
+            #print(text)
             if x0 < 18 and x0 > 17 and text:
                 results_bene.append([x0, y0, x1, y1, text])
                 #print(f"** Match Found Bene ** Coordinates: ({x0}, {y0}, {x1}, {y1}) - Text: {text}")
@@ -72,8 +75,12 @@ def annotate_pdf_with_text(xml_path, input_pdf_path, output_pdf_path):
             # 360.65 493.303 402.947 502.253
             # 368.15 511.303 402.983 520.253
 
-            # print(text_x, text_y, x1, y1, text)
-        #
+            if text:
+                all_text.append([text_x, text_y, x1, y1, text])
+
+        print(all_text)
+        filtered_arrays = [array for array in all_text if len(array) >= 5 and array[4] == "ISIN"]
+        print(filtered_arrays)
         # print(results_pos)
         # print(results_bene)
         print("\n" + "=" * 40 + "\n")
@@ -98,16 +105,17 @@ def annotate_pdf_with_text(xml_path, input_pdf_path, output_pdf_path):
 
         # Display the merged DataFrame
         if not merged_df.empty:
-            print('')
             print(merged_df[['text_pos', 'text_bene']])
             results_bene = []
             results_pos = []
 
-            can.save()
-            packet.seek(0)
-            new_pdf = PdfReader(packet)
-            new_page = new_pdf.pages[0]
-            pdf_writer.add_page(new_page)
+        can.save()
+        packet.seek(0)
+        new_pdf = PdfReader(packet)
+        new_page = new_pdf.pages[0]
+        pdf_writer.add_page(new_page)
+
+        all_text = []
 
     with open(output_pdf_path, 'wb') as output_pdf:
         pdf_writer.write(output_pdf)
